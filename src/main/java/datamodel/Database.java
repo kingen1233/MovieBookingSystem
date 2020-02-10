22e54217eb7a980ad61f343f97628e6c54c9a976
André <Andr@192.168.1.66>
2020-02-10 23:22:57 +0100

    Completed

---
+++ src/main/java/datamodel/Database.java
@@ -0,0 +1,373 @@
+package datamodel;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Database is a class that specifies the interface to the movie database. Uses
+ * JDBC and the MySQL Connector/J driver.
+ */
+public class Database {
+	/**
+	 * The database connection.
+	 */
+	private Connection conn;
+
+	/**
+	 * Create the database interface object. Connection to the database is performed
+	 * later.
+	 */
+	public Database() {
+		conn = null;
+	}
+
+	/* Change this method to fit your choice of DBMS --- */
+	/**
+	 * Open a connection to the database, using the specified user name and
+	 * password.
+	 *
+	 * @param userName The user name.
+	 * @param password The user's password.
+	 * @return true if the connection succeeded, false if the supplied user name and
+	 *         password were not recognized. Returns false also if the JDBC driver
+	 *         isn't found.
+	 */
+	public boolean openConnection(String userName, String password) {
+		try {
+			// Connection strings for included DBMS clients:
+			// [MySQL] jdbc:mysql://[host]/[database]
+			// [PostgreSQL] jdbc:postgresql://[host]/[database]
+			// [SQLite] jdbc:sqlite://[filepath]
+
+			// Use "jdbc:mysql://puccini.cs.lth.se/" + userName if you using our shared
+			// server
+			// If outside, this statement will hang until timeout.
+			conn = DriverManager.getConnection("jdbc:sqlite://C:/Users/André/Desktop/Programmering/Databas/Lab3.db",
+					userName, password);
+
+		} catch (SQLException e) {
+			System.err.println(e);
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Close the connection to the database.
+	 */
+	public void closeConnection() {
+		try {
+			if (conn != null)
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conn = null;
+
+		System.err.println("Database connection closed.");
+	}
+
+	/**
+	 * Check if the connection to the database has been established
+	 *
+	 * @return true if the connection has been established
+	 */
+	public boolean isConnected() {
+		return conn != null;
+	}
+
+	public Show getShowData(String mTitle, String mDate) {
+		Integer mFreeSeats = null;
+		String mVenue = null;
+
+		/* add code for database query --- */
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			String sql = "SELECT theaterName, emptySeats\n" + "FROM MovieShowing\n" + "WHERE movieName = \"" + mTitle
+					+ "\" AND dateOf = \"" + mDate + "\"\n" + "ORDER BY dateOf";
+
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				mVenue = rs.getString("theaterName");
+				mFreeSeats = rs.getInt("emptySeats");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return new Show(mTitle, mDate, mVenue, mFreeSeats);
+	}
+
+	private int getNextReservationnbr() {
+
+		int resNbr = 0;
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			String sql = "SELECT reservationNumber\n" + "FROM Ticket\n" + "ORDER BY reservationNumber DESC";
+
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			try {
+				resNbr = rs.getInt("reservationNumber") + 1;
+			} catch (SQLException e) {
+				resNbr = 0;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return resNbr;
+	}
+
+	public Reservation makeReservation(Show showing) {
+
+		int reservationNbr = getNextReservationnbr();
+		int showingID = getShowingID(showing.getTitle(), showing.getDate());
+		String currentUser = CurrentUser.instance().getCurrentUserId();
+
+		PreparedStatement ps = null;
+
+		try {
+			String sql = "insert into Ticket(reservationNumber, username, showingID, movieName, theaterName) "
+					+ "values (?, ?, ?, ? ,?)";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, reservationNbr);
+			ps.setString(2, currentUser);
+			ps.setInt(3, showingID);
+			ps.setString(4, showing.getTitle());
+			ps.setString(5, showing.getVenue());
+
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		Reservation reserv = new Reservation(reservationNbr, showing.getTitle(), showing.getDate(), showing.getVenue());
+		reserveSeat(showing);
+		return reserv;
+	}
+
+	private void reserveSeat(Show showing) {
+
+		try {
+			Statement stmt = conn.createStatement();
+			String sql = "UPDATE MovieShowing\n" + "SET emptySeats = emptySeats - 1\n" + "WHERE movieName = \""
+					+ showing.getTitle() + "\" AND dateOf = \"" + showing.getDate() + "\"";
+			stmt.executeUpdate(sql);
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean getUser(String userName) {
+
+		String username = null;
+		/* add code for database query --- */
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			String sql = "SELECT username\n" + "FROM User\n" + "WHERE username = \"" + userName + "\"";
+
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				username = rs.getString("username");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return username.equals(userName);
+	}
+
+	public ArrayList<String> getMovies() {
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		ArrayList<String> list = new ArrayList<String>();
+
+		try {
+			String sql = "SELECT * \n" + "FROM Movie\n" + "ORDER BY movieName";
+
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				String movieName = rs.getString("movieName");
+				list.add(movieName);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return list;
+
+	}
+
+	public ArrayList<String> getShowingDate(String mName) {
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		ArrayList<String> list = new ArrayList<String>();
+
+		try {
+			String sql = "SELECT dateOf\n" + "FROM MovieShowing\n" + "WHERE movieName = \"" + mName + "\"\n"
+					+ "ORDER BY dateOf";
+
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				String dateOf = rs.getString("dateOf");
+				list.add(dateOf);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return list;
+
+	}
+	
+	public ArrayList<Reservation> getBookings() {
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		ArrayList<Reservation> list = new ArrayList<Reservation>();
+
+		try {
+			String sql = "SELECT reservationNumber, Ticket.movieName, dateOf, Ticket.theaterName\n" + 
+					"FROM Ticket, MovieShowing\n" + 
+					"WHERE Ticket.movieName = MovieShowing.movieName AND Ticket.showingID = MovieShowing.showingID";
+
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				int reservationNumber = rs.getInt("reservationNumber");
+				String mName = rs.getString("movieName");
+				String dateOf = rs.getString("dateOf");
+				String theaterName = rs.getString("theaterName");
+				list.add(new Reservation(reservationNumber, mName, dateOf, theaterName));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return list;
+
+	}
+
+	public Integer getShowingID(String mTitle, String mDate) {
+
+		Integer showingID = 0;
+
+		/* add code for database query --- */
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			String sql = "SELECT showingID\n" + "FROM MovieShowing\n" + "WHERE movieName = \"" + mTitle
+					+ "\" AND dateOf = \"" + mDate + "\"";
+
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			showingID = rs.getInt("showingID");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return showingID;
+	}
+}
---
+++ src/main/java/gui/BookingTab.java
@@ -0,0 +1,167 @@
+package gui;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+import java.util.List;
+
+import datamodel.CurrentUser;
+import datamodel.Database;
+import datamodel.Reservation;
+import datamodel.Show;
+
+import java.util.ArrayList;
+
+
+public class BookingTab {
+	// top context message
+	@FXML private Text topContext;
+	// bottom message
+	@FXML private Text bookMsg;
+	
+	// table references
+	@FXML private ListView<String> moviesList;
+	@FXML private ListView<String> datesList;
+	
+	// show info references
+	@FXML private Label showTitle;
+	@FXML private Label showDate;
+	@FXML private Label showVenue;
+	@FXML private Label showFreeSeats;
+	
+	// booking button
+	@FXML private Button bookTicket;
+	
+	private Database db;
+	private Show crtShow = new Show();
+	
+	public void initialize() {
+		System.out.println("Initializing BookingTab");
+				
+		// set up listeners for the movie list selection
+		moviesList.getSelectionModel().selectedItemProperty().addListener(
+				(obs, oldV, newV) -> {
+					// need to update the date list according to the selected movie
+					// update also the details on the right panel
+					String movie = newV;
+					fillDatesList(newV);
+					fillShow(movie,null);
+				});
+		
+		// set up listeners for the date list selection
+		datesList.getSelectionModel().selectedItemProperty().addListener(
+				(obs, oldV, newV) -> {
+					// need to update the details according to the selected date
+					String movie = moviesList.getSelectionModel().getSelectedItem();
+					String date = newV;
+				    fillShow(movie, date);
+				});
+
+		// set up booking button listener
+		// one can either use this method (setup a handler in initialize)
+		// or directly give a handler name in the fxml, as in the LoginTab class
+		bookTicket.setOnAction(
+				(event) -> {
+					String movie = moviesList.getSelectionModel().getSelectedItem();
+					String date = datesList.getSelectionModel().getSelectedItem();
+					/* --- TODO: should attempt to book a ticket via the database --- */
+					/* --- do not forget to report booking number! --- */
+					/* --- update the displayed details (free seats) --- */
+					
+					Show showing = db.getShowData(movie, date);
+					
+					
+					
+					if(showing.getSeats() > 0) {						
+						Reservation reserv = db.makeReservation(showing);
+						report("Booked one ticket to "+movie+" on "+date + " with booking number " +  reserv.getBookingId());
+						fillShow(movie, date); //Updates show info
+					}
+					
+					else {
+						Alert alert = new Alert(AlertType.INFORMATION);
+						alert.setTitle("Fully booked!");
+						alert.setHeaderText(null);
+						alert.setContentText("Selected showing is fully booked");
+						alert.showAndWait();
+					}
+					
+					
+					
+					
+				});
+		
+		report("Ready.");
+	}
+	
+	// helpers	
+	// updates user display
+	private void fillStatus(String usr) {
+		if(usr.isEmpty()) topContext.setText("You must log in as a known user!");
+		else topContext.setText("Currently logged in as " + usr);
+	}
+	
+	private void report(String msg) {
+		bookMsg.setText(msg);
+	}
+	
+	public void setDatabase(Database db) {
+		this.db = db;
+	}
+	
+	private void fillNamesList() {
+		List<String> allmovies = new ArrayList<String>();
+		
+		for(String s : db.getMovies()) {
+			allmovies.add(s);
+		}		
+
+		moviesList.setItems(FXCollections.observableList(allmovies));
+		// remove any selection
+		moviesList.getSelectionModel().clearSelection();
+	}
+
+	private void fillDatesList(String m) {
+		List<String> alldates = new ArrayList<String>();
+		if(m!=null) {
+			// query the database via db
+			/* replace with own code --- */
+			for(String s : db.getShowingDate(m)) {				
+				alldates.add(s);
+			}	
+		}
+		datesList.setItems(FXCollections.observableList(alldates));
+		// remove any selection
+		datesList.getSelectionModel().clearSelection();
+	}
+	
+	private void fillShow(String movie, String date) {
+		if(movie==null) // no movie selected
+			crtShow = new Show();
+		else if(date==null) // no date selected yet
+			crtShow = new Show(movie);
+		else // query the database via db
+			crtShow = db.getShowData(movie, date);
+		
+		showTitle.setText(crtShow.getTitle());
+		showDate.setText(crtShow.getDate());
+		showVenue.setText(crtShow.getVenue());
+		if(crtShow.getSeats() >= 0) showFreeSeats.setText(crtShow.getSeats().toString());
+		else showFreeSeats.setText("-");
+	}
+	
+	// called in case the user logged in changed
+	public void userChanged() {
+		fillStatus(CurrentUser.instance().getCurrentUserId());
+		fillNamesList();
+		fillDatesList(null);
+		fillShow(null,null);
+	}
+	
+}
